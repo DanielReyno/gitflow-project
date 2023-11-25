13e6e9e8cf4d1d0302c9e4e1fc10268c0e6012bf
Daniel <DanielSebastianrf@gmail.com>
2023-11-25 10:38:28 -0400

    Extraer la info desde la base de datos

--- gitflow-project/gitflow-project/Form1.cs
+++ gitflow-project/gitflow-project/Form1.cs
@@ -12,9 +12,26 @@ namespace gitflow_project
 {
     public partial class Form1 : Form
     {
+        database db = new database();
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        void llenarTabla()
+        {
+            DataTable dt = db.llenarTabla();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            llenarTabla();
         }
     }
 }
